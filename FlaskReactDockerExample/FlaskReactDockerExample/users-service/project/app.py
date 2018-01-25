7b9e5c0617326727c8ae7c9671d82493cdb38e28
Robert Coleman <rocolema@microsoft.com>
2018-01-25 12:55:13 -0800

    don't print config
    don't print config

--- FlaskReactDockerExample/FlaskReactDockerExample/users-service/project/app.py
+++ FlaskReactDockerExample/FlaskReactDockerExample/users-service/project/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
-print(app.config, file=sys.stderr)
+#print(app.config, file=sys.stderr)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():

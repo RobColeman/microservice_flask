0dc74e6ae878cd652f8e87a6b1392022b2d2f960
Robert Coleman <rocolema@microsoft.com>
2018-01-25 12:36:13 -0800

    docker container for users service
    docker container for users service

--- FlaskReactDockerExample/FlaskReactDockerExample/users-service/project/app.py
+++ FlaskReactDockerExample/FlaskReactDockerExample/users-service/project/app.py
@@ -1,12 +1,17 @@
 # users-service/project/app.py
 
 from flask import Flask, jsonify
+import os, sys
 
 # instantiate the app
 
 app = Flask(__name__)
 
-app.config.from_object('project.config.DevelopmentConfig')
+#app.config.from_object('project.config.DevelopmentConfig')
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+print(app.config, file=sys.stderr)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():

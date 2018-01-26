1976094e845b895842ad74801aa55852ec10170c
Robert Coleman <rocolema@microsoft.com>
2018-01-25 16:57:56 -0800

    user db
    user db

--- FlaskReactDockerExample/FlaskReactDockerExample/users-service/project/app.py
+++ FlaskReactDockerExample/FlaskReactDockerExample/users-service/project/app.py
@@ -1,7 +1,9 @@
 # users-service/project/app.py
 
+import os, sys, datetime
 from flask import Flask, jsonify
-import os, sys
+from flask_sqlalchemy import SQLAlchemy
+
 
 # instantiate the app
 
@@ -12,6 +14,22 @@ app = Flask(__name__)
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
 #print(app.config, file=sys.stderr)
+
+# instantiate the db
+db = SQLAlchemy(app)# User model
+class User(db.Model):
+
+    # class level fields
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        """ Object level fields"""
+        self.username = username
+        self.email = email
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():

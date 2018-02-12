1b5bd0e562f5b5ca33bcd7bd2872bf610e289f11
Rob <rbrt.coleman@gmail.com>
2018-02-11 23:45:50 -0800

    user endpoint test driven dev
    user endpoint test driven dev

--- FlaskReactDockerExample/testdriven-app/users-service/project/api/users.py
+++ FlaskReactDockerExample/testdriven-app/users-service/project/api/users.py
@@ -1,6 +1,8 @@
 # users-service/project/app.py
 
 from flask import Blueprint, jsonify, request
+from sqlalchemy import exc
+from psycopg2 import DataError
 
 from project.api.models import User
 from project import db
@@ -21,12 +23,66 @@ def ping_pong():
 @users_blueprint.route('/users', methods=['POST'])
 def add_user():
     post_data = request.get_json()
+    response_object = {
+        'status' : 'fail',
+        'message': 'Invalid payload.'
+    }
+    # ensure post data is not null
+    if not post_data:
+        return jsonify(response_object), 400
     username = post_data.get('username')
     email = post_data.get('email')
-    db.session.add(username=username, email=email)
-    db.session.commit()
+    try:
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            db.session.add(User(username=username, email=email))
+            db.session.commit()
+            response_object['status'] = 'success'
+            response_object['message'] = f'{email} was added!'
+            return jsonify(response_object), 201
+        else:
+            response_object['message'] = 'Sorry. That email already exists.'
+            return jsonify(response_object), 400
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        return jsonify(response_object), 400
+
+
+@users_blueprint.route('/users/<user_id>', methods=['GET'])
+def get_single_user(user_id):
+    """Get single user details"""
     response_object = {
-        'status' : 'success',
-        'message': f'{email} was added!'
+        'status': 'fail',
+        'message': 'User does not exist'
     }
-    return jsonify(response_object), 201
+    try:
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            return jsonify(response_object), 404
+        else:
+            response_object = {
+                'status': 'success',
+                'data': {
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email,
+                    'active': user.active
+                }
+            }
+            return jsonify(response_object), 200
+    except ValueError as e:
+        return jsonify(response_object), 404
+    except exc.DataError as e:
+        return jsonify(response_object), 404
+
+
+@users_blueprint.route('/users', methods=['GET'])
+def get_all_users():
+    """Get all users"""
+    response_object = {
+        'status': 'success',
+        'data': {
+            'users': [user.to_json() for user in User.query.all()]
+        }
+    }
+    return jsonify(response_object), 200

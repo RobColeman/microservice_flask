b6722a8ef88fe1eff9fb7895d909c5783e9c6dc2
Rob <rbrt.coleman@gmail.com>
2018-02-12 00:18:06 -0800

    seed db
    seed db

--- FlaskReactDockerExample/testdriven-app/users-service/manage.py
+++ FlaskReactDockerExample/testdriven-app/users-service/manage.py
@@ -29,5 +29,13 @@ def recreate_db():
     db.session.commit()
 
 
+@manager.command
+def seed_db():
+    """Seeds teh database"""
+    db.session.add(User(username='michael', email='michael@realpython.com'))
+    db.session.add(User(username='michaelherman', email='michael@mherman.org'))
+    db.session.commit()
+
+
 if __name__ == '__main__':
     manager.run()
